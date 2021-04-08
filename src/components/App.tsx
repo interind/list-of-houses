3497fc23940f1f63e160d639c9c6b10c27258aca
Aleksandr <interind@yandex.ru>
2021-04-08 23:19:47 +0300

    start

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        background: '#000',
+        width: '400px',
+        height: '400px',
+      }}>
+      <img src={logo} alt='logo' />
+    </div>
+  );
+}
+
+export default App;
